4df9ecb75610ec07587157392de29be267b4b555
옥서혁 <ogi080124@gmail.com>
2023-07-13 14:58:28 +0900

    설명 보충

--- app/sago/page.jsx
+++ app/sago/page.jsx
@@ -30,10 +30,10 @@ export default async function Index() {
 }*/
 
 
-import React from 'react';
+import React from 'react'; //React는 객체, react는 라이브러리 이름
 import ReactDOM from 'react-dom';
 export default function Index() {
-    const [inputValue, setInputValue] = React.useState('');
+    const [inputValue, setInputValue] = React.useState(''); //React:라이브러리, 훅: api
 
     const showInput = function () {
         const inputElement = document.getElementById("userInput")
@@ -42,11 +42,11 @@ export default function Index() {
     }
     async function postData(url, data) {
         const response = await fetch(url, {
-            method: 'POST', // *GET, POST, PUT, DELETE 등
+            method: 'POST', //fetch : promise 반환 -> promise는 이행되면 응답 객체로 반환 
             headers: {
                 'Content-Type': 'application/json',
             },
-            body: { 'name': data }, // body의 데이터 유형은 반드시 "Content-Type" 헤더와 일치해야 함
+            body: { 'name': data }, // await으로 이행될 때까지 대기되어 response 변수에는 응답 객체 저장됨(Response 객체)
         });
         return response.json(); // JSON 응답을 네이티브 JavaScript 객체로 파싱
     }
